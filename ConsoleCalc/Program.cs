70e65827f84dad1375167b139bd433a974ad0b46
Tania Katerynchuk <3598tania@gmail.com>
2020-05-12 15:04:19 +0300

    Add a console project for testing

---
+++ ConsoleCalc/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace ConsoleCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(AnalyzerClass.CheckCurrency());
+            
+
+        }
+    }
+}
