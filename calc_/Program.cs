3c2d225c017ba75657e0016203bd7aad186f6b4a
Nataliia Dyshko <nataliia.dyshko@ukr.net>
2020-05-14 20:40:57 +0300

    Fix calc_ not being tracked issue
    Now calc_ project is tracked and visible and works just fine.

---
+++ calc_/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace calc_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                string expression = args[0];
+                Console.WriteLine(AnalyzerClass.Estimate(expression));
+            }
+        }
+    }
+}
